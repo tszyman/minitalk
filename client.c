8c1f57918b33d85b09551b5ab74d3c2158cbc169
Tomasz Szymanski <tomasz_szymanski@outlook.com>
2024-08-15 21:40:28 +0200

    Added snd_msg sending a message function

--- client.c
+++ client.c
@@ -36,13 +36,38 @@ void	check_args(int argc, char **argv)
 		exit(EXIT_FAILURE);
 	}
 }
+void	snd_msg(pid_t pid, char *str)
+{
+	int		shift;
+	size_t	i;
+	size_t		len;
+
+	len = ft_strlen(str);
+	i = 0;
+	while (i <= len)
+	{
+		shift = 0;
+		while (shift < 8)
+		{
+			if ((str[i] >> shift) & 1)
+				kill(pid, SIGUSR2);
+			else
+				kill(pid, SIGUSR1);
+			usleep(100);
+			shift++;
+		}
+		i++;
+	}
+}
 
 int	main(int argc, char **argv)
 {
-	//int		pid;
-	//char	*str;
+	int		pid;
+	char	*str;
 
 	check_args(argc, argv);
-	//pid = atoi(argv[1]);
+	pid = atoi(argv[1]);
+	str = argv[2];
+	snd_msg(pid, str);
 	return (EXIT_SUCCESS);
 }
